b4c86322dd2d28c52b350673088b2de337c7df21
Christofer Carlsson <christofer.carlsson@medieinstitutet.se>
2019-10-31 15:04:40 +0100

    vi lade till storyn i ett eget dokument, och fixade fel inmatining

--- adventure.js
+++ adventure.js
@@ -1,5 +1,5 @@
 let theText = document.getElementById("text"); //The text
-let singleButtons = document.getElementsByTagName("button"); //The button
+let singleButtons = document.getElementById("btn"); //The button
 let enteredText = ""; //We create an empty variable that will store the text the user enters
 let playerClass; //The players class
 let stage = 1; //The stage we are on
@@ -26,6 +26,12 @@ function selectOption(){
      * We store all the stages and what will happen next
      */
     switch(stage) {
+        case 0:
+            theText.innerHTML = "I'm sorry, what was that?";
+            singleButtons.innerHTML = "Stop messing around";
+            stage = 1;
+            break;
+
         case 1:
             theText.innerHTML = "What is your name?";
             singleButtons.innerHTML = "Submit Name";
@@ -34,6 +40,11 @@ function selectOption(){
 8
         case 2:
             const playerName = enteredText;
+            if (playerName == ""){
+                stage = 0;
+                selectOption();
+                break;
+            }
             theText.innerHTML = "Greetings " + playerName + "! Did you grow up as a rouge, a warrior, or a wizard?";
             singleButtons.innerHTML = "Submit Class";   
             stage = 3;         
@@ -60,31 +71,41 @@ function selectOption(){
             break;
 
         case 4:
-            theText.innerHTML = "You grew up as a mercenary, fighting for your life. You wielded your sword like your life depended on it. You are strong and fearless, just like a true Warrior.";
+            theText.innerHTML = warrior;
             singleButtons.innerHTML = "Yeah!";
             stage = 7;
             break;
 
         case 5:
-            theText.innerHTML = "You grew up in the shadows, hiding and stealing what you needed to survive. You know how to stalk your prey, and are not afraid to prove it.";
+            theText.innerHTML = rouge;
             singleButtons.innerHTML = "Yeah!";
             stage = 7;
             break;
 
         case 6:
-            theText.innerHTML = "You grew up with your nose in the books, always studying for the next Wizard exam. However you wish to prove your magical skills, and set out on a journey.";
+            theText.innerHTML = wizard;
             singleButtons.innerHTML = "Yeah!";
             stage = 7;
             break;
 
         case 7:
-            theText.innerHTML = "You have wandered for three days, with nothing but an old map as your guide."
-            singleButtons.innerHTML = "My feet hurt.."
-            stage = 8;
+            theText.innerHTML = wanderingPartOne;
+            singleButtons.innerHTML = "My feet hurt..";
+            if(playerClass == "Rouge") {
+                stage = 8;
+            }
+            else{
+                stage = 9;
+            }
             break;
 
         case 8:
-            theText.innerHTML = "You have gotten the map from an old man living in a nearby village. You should have probably understood by the crazy babbling that the old man was plain mad. However you couldn't resist the opportunity of an adventure.. Even if it cost you your last gold."
+            theText.innerHTML = wanderingPartTwoRouge;
+            singleButtons.innerHTML = "A piece of cake";
+            break;
+
+        case 9:
+            theText.innerHTML = wanderingPartTwo;
             singleButtons.innerHTML = "I should maintain my finances better"
             break;
     }
---
+++ theStory.js
@@ -0,0 +1,14 @@
+const warrior = "You grew up as a mercenary, fighting for your life. You wielded your sword like your life depended on it. You are strong and fearless, just like a true Warrior.";
+
+const rouge = "You grew up in the shadows, hiding and stealing what you needed to survive. You know how to stalk your prey, and are not afraid to prove it.";
+
+const wizard = "You grew up with your nose in the books, always studying for the next Wizard exam. However you wish to prove your magical skills, and set out on a journey.";
+
+const wanderingPartOne = "You have wandered for three days, with nothing but an old map as your guide.";
+
+
+const wanderingPartTwo = "You have gotten the map from an old man living in a nearby village. You should have probably understood by the crazy babbling that the old man was plain mad. However you couldn't resist the opportunity of an adventure.. Even if it cost you your last gold.";
+
+const wanderingPartTwoRouge = "You have gotten the map from an old man living in a nearby village. You should have probably understood by the crazy babbling that the old man was plain mad. His screaming and cackling made stealing the map a childs play"
+
+const wanderingPartThree = "";
