6983e6eea3dc806e340e6ca713e4e265aea241ce
Christofer Carlsson <christofer.carlsson@medieinstitutet.se>
2019-10-31 14:29:54 +0100

    Vi har fixat så att spelet väljer rätt väg för rätt val

--- adventure.js
+++ adventure.js
@@ -9,6 +9,7 @@ let stage = 1; //The stage we are on
  */
 function submit(){
     enteredText = document.getElementById("enterText").value;
+    document.getElementById("enterText").value = ""; //We remove the text from the box after the user presses the button
     selectOption();
     
 }
@@ -52,22 +53,39 @@ function selectOption(){
                 playerClass = "Wizard";
                 stage = 6;
             }
+            else{
+                theText.innerHTML = "Please enter a valid class! Choose between Warrior, Rouge, or Wizard!"
+            }
+            selectOption();
             break;
 
         case 4:
-            theText.innerHTML = "You are a Warrior";
+            theText.innerHTML = "You grew up as a mercenary, fighting for your life. You wielded your sword like your life depended on it. You are strong and fearless, just like a true Warrior.";
             singleButtons.innerHTML = "Yeah!";
+            stage = 7;
             break;
 
         case 5:
-            theText.innerHTML = "You are a Rouge";
+            theText.innerHTML = "You grew up in the shadows, hiding and stealing what you needed to survive. You know how to stalk your prey, and are not afraid to prove it.";
             singleButtons.innerHTML = "Yeah!";
+            stage = 7;
             break;
 
         case 6:
-            theText.innerHTML = "You are a Wizard";
+            theText.innerHTML = "You grew up with your nose in the books, always studying for the next Wizard exam. However you wish to prove your magical skills, and set out on a journey.";
             singleButtons.innerHTML = "Yeah!";
+            stage = 7;
             break;
 
+        case 7:
+            theText.innerHTML = "You have wandered for three days, with nothing but an old map as your guide."
+            singleButtons.innerHTML = "My feet hurt.."
+            stage = 8;
+            break;
+
+        case 8:
+            theText.innerHTML = "You have gotten the map from an old man living in a nearby village. You should have probably understood by the crazy babbling that the old man was plain mad. However you couldn't resist the opportunity of an adventure.. Even if it cost you your last gold."
+            singleButtons.innerHTML = "I should maintain my finances better"
+            break;
     }
 }
