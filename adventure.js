661237cd53aff74fbfb79ba389d6e6fe4861fd7f
Christofer Carlsson <christofer.carlsson@medieinstitutet.se>
2019-11-04 08:42:07 +0100

    Lade till styling

--- adventure.js
+++ adventure.js
@@ -3,6 +3,7 @@ let singleButtons = document.getElementById("btn"); //The button
 let enteredText = ""; //We create an empty variable that will store the text the user enters
 let playerClass; //The players class
 let stage = 1; //The stage we are on
+let marking = true; //The marking is "on"
 
 /**
  * The user presses the button
@@ -26,28 +27,25 @@ function selectOption(){
      * We store all the stages and what will happen next
      */
     switch(stage) {
-        case 0:
-            theText.innerHTML = "I'm sorry, what was that?";
-            singleButtons.innerHTML = "Stop messing around";
-            stage = 1;
-            break;
 
         case 1:
-            theText.innerHTML = "What is your name?";
-            singleButtons.innerHTML = "Submit Name";
+            theText.innerHTML = "What is your name?";  //Changes the text
+            singleButtons.innerHTML = "Submit Name";   //Changes the button text
             stage = 2;
             break;
 8
         case 2:
             const playerName = enteredText;
             if (playerName == ""){
-                stage = 0;
-                selectOption();
+                theText.innerHTML = "I'm sorry, what was that?";
+                singleButtons.innerHTML = "Stop messing around";
                 break;
             }
-            theText.innerHTML = "Greetings " + playerName + "! Did you grow up as a rouge, a warrior, or a wizard?";
-            singleButtons.innerHTML = "Submit Class";   
-            stage = 3;         
+            else{
+                theText.innerHTML = "Greetings " + playerName + "! Did you grow up as a rouge, a warrior, or a wizard?";
+                singleButtons.innerHTML = "Submit Class";   
+                stage = 3;
+            }         
             break;
             
         case 3:
@@ -66,6 +64,7 @@ function selectOption(){
             }
             else{
                 theText.innerHTML = "Please enter a valid class! Choose between Warrior, Rouge, or Wizard!"
+                break;
             }
             selectOption();
             break;
@@ -102,11 +101,106 @@ function selectOption(){
         case 8:
             theText.innerHTML = wanderingPartTwoRouge;
             singleButtons.innerHTML = "A piece of cake";
+            stage = 10;
             break;
 
         case 9:
             theText.innerHTML = wanderingPartTwo;
             singleButtons.innerHTML = "I should maintain my finances better"
+            stage = 10;
+            break;
+
+        case 10:
+            theText.innerHTML = wanderingPartThree;
+            singleButtons.innerHTML = "Finally";
+            stage = 11;
+            break;
+            
+        case 11:
+            theText.innerHTML = caveEnteranceOne;
+            singleButtons.innerHTML = "How peculiar";
+            stage = 12;
+            document.body.style.backgroundImage = "url('outsideCave.jpg')";
+            break;
+
+        case 12:
+            theText.innerHTML = caveEnteranceTwo;
+            singleButtons.innerHTML = "Hmm..";
+            stage = 13;
+            break;
+
+        case 13:
+            if (enteredText.toLowerCase() == "yes" && playerClass == "Wizard")
+            {
+                stage = 14;
+            }
+            else if (enteredText.toLowerCase() == "yes" && playerClass != "Wizard")
+            {
+                stage = 18;
+            }
+            else if (enteredText.toLowerCase() == "no")
+            {
+                stage = 19;
+            }
+            else
+            {
+                theText.innerHTML = "Please enter yes or no";
+                singleButtons.innerHTML = "Come on, dude..";
+                break;
+            }
+            selectOption();
+            break;
+
+        case 14:
+            theText.innerHTML = caveEnteranceThreeYesWizard;
+            singleButtons.innerHTML = "Interesting"
+            stage = 15;
+            break;
+
+        case 15:
+            theText.innerHTML = caveEnteranceThreeYesWizardQuestion;
+            singleButtons.innerHTML = "What to do.."
+            stage = 16;
+            break;
+
+        case 16:
+            if (enteredText.toLowerCase() == "yes") {
+                stage = 17;
+            }
+            else if (enteredText.toLowerCase() == "no") {
+                stage = 20;
+            }
+            else {
+                theText.innerHTML = "Please enter Yes or No";
+                singleButtons.innerHTML = "Make a decision"
+                break;
+            }
+            selectOption();
+            break;
+
+        case 17:
+            theText.innerHTML = caveEnteranceThreeWizardTouch;
+            singleButtons.innerHTML = "Now what did that activate?"
+            stage = 20;
+            marking = false;
+            break;
+
+        case 18:
+            theText.innerHTML = caveEnteranceThreeYes;
+            singleButtons.innerHTML = "What did that do?"
+            stage = 20;
+            marking = false;
+            break;
+
+        case 19:
+            theText.innerHTML = caveEnteranceThreeNo;
+            singleButtons.innerHTML = "Let's look around"
+            stage = 20;
+            break;
+
+        case 20:
+            theText.innerHTML = caveEnteranceFour;
+            singleButtons.innerHTML = "Coming soon"
             break;
     }
 }
--- theStory.js
+++ theStory.js
@@ -1,14 +1,45 @@
-const warrior = "You grew up as a mercenary, fighting for your life. You wielded your sword like your life depended on it. You are strong and fearless, just like a true Warrior.";
+const warrior = "You grew up as a mercenary, fighting for your life. You wielded your sword like your life depended on it. \
+You are strong and fearless, just like a true Warrior.";
 
-const rouge = "You grew up in the shadows, hiding and stealing what you needed to survive. You know how to stalk your prey, and are not afraid to prove it.";
+const rouge = "You grew up in the shadows, hiding and stealing what you needed to survive. \
+You know how to stalk your prey, and are not afraid to prove it.";
 
-const wizard = "You grew up with your nose in the books, always studying for the next Wizard exam. However you wish to prove your magical skills, and set out on a journey.";
+const wizard = "You grew up with your nose in the books, always studying for the next Wizard exam. \
+However you wish to prove your magical skills, and set out on a journey.";
 
 const wanderingPartOne = "You have wandered for three days, with nothing but an old map as your guide.";
 
 
-const wanderingPartTwo = "You have gotten the map from an old man living in a nearby village. You should have probably understood by the crazy babbling that the old man was plain mad. However you couldn't resist the opportunity of an adventure.. Even if it cost you your last gold.";
+const wanderingPartTwo = "You have gotten the map from an old man living in a nearby village. \
+You should have probably understood by the crazy babbling that the old man was plain mad. \
+However you couldn't resist the opportunity of an adventure.. Even if it cost you your last gold.";
 
-const wanderingPartTwoRouge = "You have gotten the map from an old man living in a nearby village. You should have probably understood by the crazy babbling that the old man was plain mad. His screaming and cackling made stealing the map a childs play"
+const wanderingPartTwoRouge = "You have gotten the map from an old man living in a nearby village. \
+You should have probably understood by the crazy babbling that the old man was plain mad. \
+His screaming and cackling made stealing the map a childs play"
 
-const wanderingPartThree = "";
+const wanderingPartThree = "After what feels like a week of wandering, traversing the forest and rocky mountains, you finally find what you are looking for.";
+
+const caveEnteranceOne = "You find yourself outside a peculiar looking cave. \
+The cave does look exacly like the one that is marked on your map, \
+with the same markings of a three winged raven that was described on the map."
+
+const caveEnteranceTwo = "The three winged raven is marked on the stone in what seems to be a glowing blue color. Do you wish to examine it?"
+
+const caveEnteranceThreeYes = "You reach out and touch the symbol, and as your fingers connect with the blue liquid, \
+the glow starts to fade from it until the color is completely gone. \
+You wish that you knew what it was before touching it."
+
+const caveEnteranceThreeYesWizard = "You carefully examine the symbol, careful not to touch it. \
+You recognise the liquid to be something called illumious and are used for some greater illusion spells. \
+It seems like this one has been made to active something when touching it."
+
+const caveEnteranceThreeYesWizardQuestion = "Do you wish to touch it?"
+
+const caveEnteranceThreeWizardTouch = "You reach out and touch the symbol with the palm of your hand, \
+knowing fully well that somehting would activate. As your palm make contact with the symbol, the color fades and the marking becomes dark.\
+Something has activated, but you are not sure what."
+
+const caveEnteranceThreeNo = "You decide to leave the strange marking alone and look around instead."
+
+const caveEnteranceFour = "Coming soon"
